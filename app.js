560d3996b3b9a468e9f0b90790e53277cd78741b
artsem0214 <artsem0214@protonmail.com>
2020-02-21 01:43:00 +0300

    Add tooltip for arc layer and scatter layer

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ import { StaticMap } from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
 import { ScatterplotLayer, ArcLayer } from '@deck.gl/layers';
 import CSVDATA from './assets/data.csv';
+import $ from 'jquery';
 
-// Set your mapbox token here
-const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+const MAPBOX_TOKEN = process.env.MapboxAccessToken;
 
 let INITIAL_VIEW_STATE = {
   longitude: -74,
@@ -16,6 +17,9 @@ let INITIAL_VIEW_STATE = {
   pitch: 0,
   bearing: 0
 };
+
+let tooltip = $(".tooltip")[0];
+
 
 export default class App extends Component {
 
@@ -30,7 +34,6 @@ export default class App extends Component {
   componentWillMount() {
     this.arcData = this._getArcData(CSVDATA);
     this.scatterplotData = this._getScatterplotData(CSVDATA);
-
     //initialize lon & lat
     for (let i = 0; this.scatterplotData.length; i++) {
       if (this.scatterplotData[i].coordinates) {
@@ -48,12 +51,13 @@ export default class App extends Component {
         id: 'scatter-plot',
         data: this.scatterplotData,
         pickable: true,
-        radiusScale: 300,
+        radiusScale: 500,
         getRadius: d => d.count,
         getPosition: d => d.coordinates,
-        getColor: d => [0, 50, 90, 180],
+        getColor: d => [100, 0, 90, 180],
         autoHighlight: true,
-        highlightColor: [50, 12, 120, 100]
+        highlightColor: [50, 12, 120, 100],
+        onHover: this._scatterplotTooltip,
       }),
       new ArcLayer({
         id: 'arc',
@@ -62,7 +66,9 @@ export default class App extends Component {
         getTargetPosition: d => d.targetPosition,
         getSourceColor: [120, 12, 50, 100],
         getTargetColor: [50, 12, 120, 100],
-        getWidth: 5
+        getWidth: 10,
+        pickable: true,
+        onHover: this._arcTooltip,
       })
     ];
   }
@@ -83,9 +89,10 @@ export default class App extends Component {
   }
 
 
-  ///////////////////////////////////////////////////////////////
-  //Create the dataset for the arc & scatterplot
-  ///////////////////////////////////////////////////////////////
+  /**
+   * Create the dataset for the arc & scatterplot
+   */
+
   // application_num: 1
   // decision_date: "2018-10-20"
   // offloading_port_cleaned_EN: "Mukalla, Yemen"
@@ -98,6 +105,7 @@ export default class App extends Component {
   // dest_lat: 49.127197
   // dest_lon: 14.5404328
   // Destination: "Mukalla, Yemen"
+
   _getArcData(data) {
     let arcData = [];
     for (let i = 0; i < data.length; i++) {
@@ -105,19 +113,29 @@ export default class App extends Component {
       const sourcePosition = [data[i].lon, data[i].lat, 0];
       const destination = data[i].Destination;
       const targetPosition = [data[i].dest_lon, data[i].dest_lat, 0];
+
+      const decisionDate = data[i].decision_date;
+      const fuelAmount = data[i].fuel_amount_clean;
+      const fuelType = data[i].fuel_type_clean_EN;
+
       if (origin !== null & sourcePosition !== null & destination !== null & targetPosition !== null) {
         arcData.push({
+          origin: origin,
           sourcePosition: sourcePosition,
-          targetPosition: targetPosition
+          destination: destination,
+          targetPosition: targetPosition,
+          decisionDate: decisionDate,
+          fuelAmount: fuelAmount,
+          fuelType: fuelType
         })
       }
     }
     return arcData;
   }
+
   _getScatterplotData(data) {
 
     const ports = {};
-    const group = [];
     for (let i = 0; i < data.length; i++) {
       const origin = data[i].Origin;
       const sourcePosition = [data[i].lon, data[i].lat, 0];
@@ -147,6 +165,43 @@ export default class App extends Component {
     return portsMod;
   }
 
+
+  /**
+   * Tooltip
+   */
+
+  //Scatterplot Tooltip
+  _scatterplotTooltip({ x, y, object }) {
+    if (object) {
+      tooltip.style.top = `${y}px`;
+      tooltip.style.left = `${x}px`;
+      tooltip.innerHTML = `<div><span class="key key-route">Port:</span><span class="value">${object.port}</span></div>`;
+      tooltip.innerHTML += `<div><span class="key key-route">Count:</span><span class="value">${object.count}</span></div>`;
+      tooltip.style.backgroundColor = '#1D1E27';
+      tooltip.style.opacity = '1';
+    } else {
+      tooltip.innerHTML = '';
+      tooltip.style.opacity = '0';
+    }
+  }
+
+  //Arc Tooltip
+  _arcTooltip({ x, y, object }) {
+    if (object) {
+      tooltip.style.top = `${y}px`;
+      tooltip.style.left = `${x}px`;
+      tooltip.innerHTML = `<div><span class="key key-route">Origin:</span><span class="value">${object.origin}</span></div>`;
+      tooltip.innerHTML += `<div><span class="key key-route">Destination:</span><span class="value">${object.destination}</span></div>`;
+      tooltip.innerHTML += `<div><span class="key key-route">Date:</span><span class="value">${new Date(object.decisionDate).toLocaleDateString()}</span></div>`;
+      tooltip.innerHTML += `<div><span class="key key-route">Fuel Type:</span><span class="value">${object.fuelType || 'Untitled'}</span></div>`;
+      tooltip.innerHTML += `<div><span class="key key-route">Fuel Amount:</span><span class="value">${object.fuelAmount || '0'}</span></div>`;
+      tooltip.style.backgroundColor = '#1D1E27';
+      tooltip.style.opacity = '1';
+    } else {
+      tooltip.innerHTML = '';
+      tooltip.style.opacity = '0';
+    }
+  }
 
 }
 
