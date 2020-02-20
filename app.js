b407bae6b482343904f56bc54a42b99805049c94
artsem0214 <artsem0214@protonmail.com>
2020-02-20 21:05:14 +0300

    Add arc and plot layer on map

--- app.js
+++ app.js
@@ -2,51 +2,67 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { StaticMap } from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
-import { ScatterplotLayer } from '@deck.gl/layers';
-import csvdata from '.assets/data.csv';
+import { ScatterplotLayer, ArcLayer } from '@deck.gl/layers';
+import CSVDATA from './assets/data.csv';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
-const MALE_COLOR = [0, 128, 255];
-const FEMALE_COLOR = [255, 0, 128];
 
-// Source data CSV
-const DATA_URL =
-  'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/scatterplot/manhattan.json'; // eslint-disable-line
-
-const INITIAL_VIEW_STATE = {
+let INITIAL_VIEW_STATE = {
   longitude: -74,
   latitude: 40.7,
-  zoom: 11,
+  zoom: 5,
   maxZoom: 16,
   pitch: 0,
   bearing: 0
 };
 
-
-
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoveredPort: null,
+      selectedPort: null
+    }
+  }
+
+  componentWillMount() {
+    this.arcData = this._getArcData(CSVDATA);
+    this.scatterplotData = this._getScatterplotData(CSVDATA);
+
+    //initialize lon & lat
+    for (let i = 0; this.scatterplotData.length; i++) {
+      if (this.scatterplotData[i].coordinates) {
+        INITIAL_VIEW_STATE.longitude = this.scatterplotData[i].coordinates[0];
+        INITIAL_VIEW_STATE.latitude = this.scatterplotData[i].coordinates[1];
+        break;
+      }
+    }
+  }
+
   _renderLayers() {
-    const {
-      data = DATA_URL,
-      radius = 30,
-      maleColor = MALE_COLOR,
-      femaleColor = FEMALE_COLOR
-    } = this.props;
 
     return [
       new ScatterplotLayer({
         id: 'scatter-plot',
-        data,
-        radiusScale: radius,
-        radiusMinPixels: 0.25,
-        getPosition: d => [d[0], d[1], 0],
-        getFillColor: d => (d[2] === 1 ? maleColor : femaleColor),
-        getRadius: 1,
-        updateTriggers: {
-          getFillColor: [maleColor, femaleColor]
-        }
+        data: this.scatterplotData,
+        pickable: true,
+        radiusScale: 300,
+        getRadius: d => d.count,
+        getPosition: d => d.coordinates,
+        getColor: d => [0, 50, 90, 180],
+        autoHighlight: true,
+        highlightColor: [50, 12, 120, 100]
+      }),
+      new ArcLayer({
+        id: 'arc',
+        data: this.arcData,
+        getSourcePosition: d => d.sourcePosition,
+        getTargetPosition: d => d.targetPosition,
+        getSourceColor: [120, 12, 50, 100],
+        getTargetColor: [50, 12, 120, 100],
+        getWidth: 5
       })
     ];
   }
@@ -65,6 +81,73 @@ export default class App extends Component {
       </DeckGL>
     );
   }
+
+
+  ///////////////////////////////////////////////////////////////
+  //Create the dataset for the arc & scatterplot
+  ///////////////////////////////////////////////////////////////
+  // application_num: 1
+  // decision_date: "2018-10-20"
+  // offloading_port_cleaned_EN: "Mukalla, Yemen"
+  // loading_port_cleaned_EN: null
+  // fuel_amount_clean: 5000
+  // fuel_type_clean_EN: "Petrol"
+  // lon: null
+  // lat: null
+  // Origin: null
+  // dest_lat: 49.127197
+  // dest_lon: 14.5404328
+  // Destination: "Mukalla, Yemen"
+  _getArcData(data) {
+    let arcData = [];
+    for (let i = 0; i < data.length; i++) {
+      const origin = data[i].Origin;
+      const sourcePosition = [data[i].lon, data[i].lat, 0];
+      const destination = data[i].Destination;
+      const targetPosition = [data[i].dest_lon, data[i].dest_lat, 0];
+      if (origin !== null & sourcePosition !== null & destination !== null & targetPosition !== null) {
+        arcData.push({
+          sourcePosition: sourcePosition,
+          targetPosition: targetPosition
+        })
+      }
+    }
+    return arcData;
+  }
+  _getScatterplotData(data) {
+
+    const ports = {};
+    const group = [];
+    for (let i = 0; i < data.length; i++) {
+      const origin = data[i].Origin;
+      const sourcePosition = [data[i].lon, data[i].lat, 0];
+      const destination = data[i].Destination;
+      const targetPosition = [data[i].dest_lon, data[i].dest_lat, 0];
+
+      if (destination in ports) {
+        ports[destination] = {
+          count: ports[destination].count + 1,
+          position: targetPosition
+        };
+      } else {
+        ports[destination] = {
+          count: 1,
+          position: targetPosition
+        };
+      }
+    }
+    const portsMod = [];
+    Object.keys(ports).forEach(function (key) {
+      portsMod.push({
+        port: key,
+        count: ports[key].count,
+        coordinates: ports[key].position
+      });
+    });
+    return portsMod;
+  }
+
+
 }
 
 export function renderToDOM(container) {
